3ddda1ec01fb615270bc67b392ab92bb435677ac
Махнатеев Степан <stepan01062004@mail.ru.>
2026-03-26 22:22:08 +0300

    Add devices storage

---
+++ internal/storage/postgres/devices.go
@@ -0,0 +1,158 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/PritOriginal/nethub-back/internal/models"
+	"github.com/PritOriginal/nethub-back/internal/storage"
+	"github.com/jmoiron/sqlx"
+)
+
+type DevicesStorage struct {
+	DB *sqlx.DB
+}
+
+func NewDeviceStorage(db *sqlx.DB) *DevicesStorage {
+	return &DevicesStorage{
+		DB: db,
+	}
+}
+
+func (s *DevicesStorage) AddDevice(ctx context.Context, device models.Device) (models.Device, error) {
+	const op = "storage.postgres.AddDevice"
+
+	var newDevice models.Device
+
+	query := `
+			INSERT INTO
+				devices (hostname, ip, location, is_active)
+			VALUES
+				(:hostname, :ip, :location, :is_active)
+			RETURNING
+				id, hostname, ip, location, is_active, created_at 
+			`
+
+	stmt, err := s.DB.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return newDevice, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := stmt.GetContext(ctx, &newDevice, device); err != nil {
+		return newDevice, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return newDevice, nil
+}
+
+func (s *DevicesStorage) GetDevices(ctx context.Context, params models.GetDevicesParams) ([]models.Device, error) {
+	const op = "storage.postgres.GetDevices"
+
+	devices := []models.Device{}
+	var conditions []string
+	var args []any
+
+	query := `
+			SELECT 
+				id, hostname, ip, location, is_active, created_at 
+			FROM
+				devices
+			WHERE
+				is_deleted = false
+			`
+	if params.Search != "" {
+		conditions = append(conditions, "hostname ILIKE $?")
+		pattern := "%" + params.Search + "%"
+		args = append(args, pattern)
+	}
+
+	if params.IsActive != nil {
+		conditions = append(conditions, "is_active = $?")
+		args = append(args, *params.IsActive)
+	}
+
+	for i, condition := range conditions {
+		query += " AND " + condition
+		query = strings.Replace(query, "$?", fmt.Sprintf("$%d", len(args)-len(conditions)+i+1), 1)
+	}
+
+	query += " ORDER BY id"
+
+	if err := s.DB.SelectContext(ctx, &devices, query, args...); err != nil {
+		return devices, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return devices, nil
+}
+
+func (s *DevicesStorage) GetDeviceById(ctx context.Context, id int) (models.Device, error) {
+	const op = "storage.postgres.GetDeviceById"
+
+	var device models.Device
+
+	query := `
+			SELECT
+				id, hostname, ip, location, is_active, created_at 
+			FROM
+				devices
+			WHERE
+				id = $1 AND is_deleted = false
+			`
+	if err := s.DB.GetContext(ctx, &device, query, id); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return device, storage.ErrNotFound
+		default:
+			return device, fmt.Errorf("%s: %w", op, err)
+		}
+	}
+
+	return device, nil
+}
+
+func (s *DevicesStorage) UpdateDevice(ctx context.Context, device models.Device) (models.Device, error) {
+	const op = "storage.postgres.UpdateDevice"
+
+	var updatedDevice models.Device
+
+	query := `
+			UPDATE 
+				devices 
+			SET 
+				hostname = :hostname, ip = :ip, location = :location, is_active = :is_active 
+			WHERE 
+				id = :id
+			RETURNING
+				id, hostname, ip, location, is_active, created_at 
+			`
+	stmt, err := s.DB.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return updatedDevice, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := stmt.GetContext(ctx, &updatedDevice, device); err != nil {
+		return updatedDevice, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return updatedDevice, nil
+}
+
+func (s *DevicesStorage) DeleteDevice(ctx context.Context, id int) error {
+	const op = "storage.postgres.DeleteDevice"
+
+	query := `
+			UPDATE
+				devices
+			SET
+				is_deleted = TRUE
+			WHERE
+				id = $1
+			`
+	if _, err := s.DB.ExecContext(ctx, query, id); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,40 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/PritOriginal/nethub-back/internal/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Postgres struct {
+	DB *sqlx.DB
+}
+
+func New(cfg config.DatabaseConfig) (*Postgres, error) {
+	const op = "storage.postgres.New"
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.Name)
+
+	db, err := sqlx.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if pingErr := db.PingContext(ctx); pingErr != nil {
+		db.Close()
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return &Postgres{DB: db}, nil
+}
+
+func (s *Postgres) Stop() error {
+	return s.DB.Close()
+}
