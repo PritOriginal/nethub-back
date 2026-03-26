9382296b8b07d9160b744b0343513973adb205df
Махнатеев Степан <stepan01062004@mail.ru.>
2026-03-26 17:11:53 +0300

    Fix down migration

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,5 +1,5 @@
-DROP INDEX IF EXISTS idx_devices_is_deleted;
+DROP INDEX IF EXISTS idx_devices_hostname_trgm;
+DROP INDEX IF EXISTS idx_devices_active_deleted_id;
 DROP INDEX IF EXISTS idx_devices_is_active;
-DROP INDEX IF EXISTS idx_devices_ip;
 
 DROP TABLE IF EXISTS devices;
